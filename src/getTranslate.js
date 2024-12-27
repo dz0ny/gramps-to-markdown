d0abf75775e4ea4ee01e4d6ecfcf074f685cc15d
Janez Troha <239513+dz0ny@users.noreply.github.com>
2024-12-27 14:32:05 +0100

    Update getTranslate.js

--- src/getTranslate.js
+++ src/getTranslate.js
@@ -1,9 +1,13 @@
 import fr from './locales/fr.js';
+import sl from './locales/sl.js';
 
 function getTranslate({ locale }) {
   let dict;
   if (locale === 'fr') {
     dict = fr;
+  }
+  if (locale === 'sl') {
+    dict = sl;
   }
   if (dict) {
     return (str) => dict[str] || str;
